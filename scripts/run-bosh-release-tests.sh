3e1dea09792c3c88a303e0ea8d586792159648d8
Dennis Leon <leonde@vmware.com>
2020-03-31 11:15:03 -0700

    Make running bosh tests not dependant on a pivotal workstation directory structure
    Authored-by: Dennis Leon <leonde@vmware.com>

--- scripts/run-bosh-release-tests.sh
+++ scripts/run-bosh-release-tests.sh
@@ -9,7 +9,7 @@ docker run \
 -i \
 --privileged \
 -e DEV=TRUE \
--v /Users/pivotal/workspace/mapfs-release:/mapfs-release \
+-v ~/workspace/mapfs-release:/mapfs-release \
 --workdir=/ \
 bosh/main-bosh-docker \
 /mapfs-release/scripts/run-bosh-release-tests-in-docker-env.sh
