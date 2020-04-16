17a6088f314bc86df906def7f6ec088f9a2b5aa5
Lisa Burns <lcho@pivotal.io>
2020-04-16 12:14:54 -0700

    Source start-bosh rather than running it in a child process
    [#172360145](https://www.pivotaltracker.com/story/show/172360145)
    
    Co-authored-by: Lisa Burns <lcho@pivotal.io>
    Co-authored-by: Paul Warren <pwarren@pivotal.io>

--- scripts/run-bosh-release-tests-in-docker-env.sh
+++ scripts/run-bosh-release-tests-in-docker-env.sh
@@ -5,18 +5,18 @@ if [[ -n "$DEV" ]]; then
     COMMAND_TO_RUN='bash'
 fi
 
-start-bosh
+export DOCKER_STORAGE_OPTIONS='--storage-opt dm.basesize=100G'
+. start-bosh
 
+source /tmp/local-bosh/director/env
 export DOCKER_TMP_DIR=$(find /tmp/ -name "tmp.*")
-export DOCKER_HOST=$(ps aux | grep dockerd | grep -o '\-\-host tcp.*4243' | awk '{print $2}')
-
-eval "$(cat /tmp/local-bosh/director/env)"
 
 docker \
 --tls \
 --tlscacert=${DOCKER_TMP_DIR}/ca.pem \
 --tlscert=${DOCKER_TMP_DIR}/cert.pem \
---tlskey=${DOCKER_TMP_DIR}/key.pem run \
+--tlskey=${DOCKER_TMP_DIR}/key.pem \
+run \
 --network=director_network \
 -v $PWD/mapfs-release:/mapfs-release \
 -v /tmp:/tmp \
